3f16a342d96740a1af10a3d50b9e49ef94aeb396
MMittenbuehler <marcel@mittenbuehler.com>
2020-02-27 23:21:02 +0100

    Initial

---
+++ precache-manifest.fcd4c40f77c2a6aeb2abfbcf2144fa4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73002791ffb83efa97312f6a1e27595a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc13d6374bfc3a269254",
+    "url": "/static/js/2.62b7928d.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.62b7928d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2a7c5970a8a93532de",
+    "url": "/static/js/main.226754ac.chunk.js"
+  },
+  {
+    "revision": "0eaa291ab6bfde2e2458",
+    "url": "/static/js/runtime-main.9cd55c1a.js"
+  }
+]);
